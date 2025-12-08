9aa9a54f262fe3fe70f3a33dd004fe085dd525bd
simbarras <simon.barras@epfl.ch>
2025-12-08 15:59:37 +0100

    lab 4

--- Lab 4/lab4_network.py
+++ Lab 4/lab4_network.py
@@ -137,6 +137,22 @@ def Lab4_Network():
     r5.cmd('sysctl -w net.ipv4.conf.r5-eth2.rp_filter=0')
     r5.cmd('sysctl -w net.ipv4.conf.r5-eth3.rp_filter=0')
 
+    # My part
+    ##########################################################################
+    r1.cmd('/usr/lib/frr/zebra -d -f /home/advnet/Desktop/lab4/configs/zebra_r1.cfg -i /home/advnet/Desktop/lab4/run/zebra_r1.pid -z /home/advnet/Desktop/lab4/run/frr_r1.api -u frr -g frr')
+    r1.cmd('/usr/lib/frr/ospfd -d -f /home/advnet/Desktop/lab4/configs/ospfd_r1.cfg -i /home/advnet/Desktop/lab4/run/ospfd_r1.pid -z /home/advnet/Desktop/lab4/run/frr_ospfd_r1.api -u frr -g frr')
+
+    r2.cmd('/usr/lib/frr/zebra -d -f /home/advnet/Desktop/lab4/configs/zebra_r2.cfg -i /home/advnet/Desktop/lab4/run/zebra_r2.pid -z /home/advnet/Desktop/lab4/run/frr_r2.api -u frr -g frr')
+    r2.cmd('/usr/lib/frr/ospfd -d -f /home/advnet/Desktop/lab4/configs/ospfd_r2.cfg -i /home/advnet/Desktop/lab4/run/ospfd_r2.pid -z /home/advnet/Desktop/lab4/run/frr_ospfd_r2.api -u frr -g frr')
+
+    r3.cmd('/usr/lib/frr/zebra -d -f /home/advnet/Desktop/lab4/configs/zebra_r3.cfg -i /home/advnet/Desktop/lab4/run/zebra_r3.pid -z /home/advnet/Desktop/lab4/run/frr_r3.api -u frr -g frr')
+    r3.cmd('/usr/lib/frr/ospfd -d -f /home/advnet/Desktop/lab4/configs/ospfd_r3.cfg -i /home/advnet/Desktop/lab4/run/ospfd_r3.pid -z /home/advnet/Desktop/lab4/run/frr_ospfd_r3.api -u frr -g frr')
+
+    r4.cmd('/usr/lib/frr/zebra -d -f /home/advnet/Desktop/lab4/configs/zebra_r4.cfg -i /home/advnet/Desktop/lab4/run/zebra_r4.pid -z /home/advnet/Desktop/lab4/run/frr_r4.api -u frr -g frr')
+    #r4.cmd('/usr/lib/frr/ospfd -d -f /home/advnet/Desktop/lab4/configs/ospfd_r4.cfg -i /home/advnet/Desktop/lab4/run/ospfd_r4.pid -z /home/advnet/Desktop/lab4/run/frr_ospfd_r4.api -u frr -g frr')
+
+    r5.cmd('/usr/lib/frr/zebra -d -f /home/advnet/Desktop/lab4/configs/zebra_r5.cfg -i /home/advnet/Desktop/lab4/run/zebra_r5.pid -z /home/advnet/Desktop/lab4/run/frr_r5.api -u frr -g frr')
+    r5.cmd('/usr/lib/frr/ospfd -d -f /home/advnet/Desktop/lab4/configs/ospfd_r5.cfg -i /home/advnet/Desktop/lab4/run/ospfd_r5.pid -z /home/advnet/Desktop/lab4/run/frr_ospfd_r5.api -u frr -g frr')
 
 
     os.system("chown advnet /home/advnet/Desktop/lab4/logs/*")
